9f5dd15f5b25cfbffe6c5de4dbd8409a6d227719
Miguel Pedregosa Pérez <mpedregosa@correo.ugr.es>
2022-04-06 20:11:23 +0200

    GM-5 WIP blocked

---
+++ src/event/event.hpp
@@ -0,0 +1,23 @@
+#ifndef __GENOMUS_CORE_EVENT__
+#define __GENOMUS_CORE_EVENT__
+
+#include <string>
+
+// namespace Event {
+//     class EventParameter {
+//         private:
+//             string name;
+//             string type;
+            
+//             float normalized_value;
+//         public:
+//             explicit EventParameter(string name, string type, float normalized_value = 0);
+//     };
+
+//     class Event {
+//         private:
+            
+//     };
+// }
+
+#endif
--- src/phenotype/phenotype.hpp
+++ src/phenotype/phenotype.hpp
@@ -54,7 +54,6 @@ namespace Phenotype {
             }
     };
 
-
     class Piano : public Species {
         public:
             Piano();   
